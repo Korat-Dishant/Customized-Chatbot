8e0ce41f33bde2b1c1f450a882ff8f99d770fe88
Dishant <koratdishant536631@gmail.com>
2023-08-18 19:01:56 +0530

    cors enabled

--- API.py
+++ API.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from OpenAI import ask_ai
 
 
+
+
 app = FastAPI(title="Customized Chatbot", description="Customized Chatbot API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/extract", summary="custom Chat bot", tags=["Chatbot"])
